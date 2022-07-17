fdf04c6f901ae9a7925ffa6a26ae103a2c6124d3
HalleyRep <44517806+aleyg@users.noreply.github.com>
2022-07-17 15:27:44 -0500

    up

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   new SweetScroll({/* some options */});
 
   /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-  particlesJS('particles-js', {
+  particlesJS('particles.js', {
     "particles": {
       "number": {
         "value": 30,
