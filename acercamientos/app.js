25154890ae45b1570f8174148a7c96d47228c240
aleyg <ale_yg@live.com>
2023-04-24 02:41:43 -0600

    'prueba'

---
+++ acercamientos/app.js
@@ -0,0 +1,11 @@
+import navbar from './components/navbar/navbar'
+import { Container } from './components/navbar/navbar.elements';
+
+function App() {
+    return <>
+        <navbar/>
+        
+    </>;
+}
+
+export default App;
---
+++ acercamientos/components/navbar/navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Container, LogoContainer, Menu, MenuItem, MenuItemLink, Wrapper } from './navbar.elements'
+
+const navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+          <LogoContainer>
+
+          </LogoContainer>
+
+          <Menu>
+            <MenuItem>
+              <MenuItemLink>
+              
+              </MenuItemLink>
+            </MenuItem>
+          </Menu>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default navbar
